b7f1cad58fd0be8dfb5b55bbad60beab9ffb61b1
Andrei <andrei.koenich@gmail.com>
2025-03-07 17:00:59 -0300

    Arquivos desnecessarios removidos

---
+++ AnalisadorGLUD.ts
@@ -0,0 +1,11 @@
+main()
+
+function iniciaLeitura(): void {
+    let fileName: string = prompt("Digite o nome do arquivo de entrada:\n")
+    console.log(fileName)
+}
+
+function main (): number {
+    iniciaLeitura()
+    return 0
+}
